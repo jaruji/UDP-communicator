ab61d9d3bc83d0dc2c30a5ffa2d63a163ede3ec5
jaruji <jurajbedej1@yahoo.com>
2019-10-02 13:48:07 +0200

    initializing winsock

--- src/communicator.c
+++ src/communicator.c
@@ -5,34 +5,51 @@
 
 #define iplen 15
 
-void server(int port){                                                                                //server side code, listening on specified port number
-    char test;
-    printf("Server listening on port: %d...\n", port);
-    while(1){                                                                                         //waiting for connection
-        scanf("%c", &test);
-        if(test == 'e'){
-            break;
-        }
+void initializeWinsock(){
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)                                  //initializing winsock
+    {
+        exit(EXIT_FAILURE);
     }
+    printf("Winsock initialized\n");
+}
+
+SOCKET createSocket(SOCKET s){
+    if((s = socket(AF_INET , SOCK_DGRAM , 0 )) == INVALID_SOCKET){
+        printf("FAILED TO CREATE SOCKET");
+        exit(EXIT_FAILURE);
+    }
+    printf("Socket succesfully created");
+    return s;
+}
+
+void server(int port){                                                                                //server side code, listening on specified port number
+    initializeWinsock();
+    SOCKET s = createSocket(s);
 }
 
 void client(char *ip, int port){
-
+    initializeWinsock();
+    SOCKET s = createSocket(s);
+    printf("Sending message to port %d, ip %s", port, ip);
 }
 
 int main(){
     char mode;
-    printf("Select mode by typing one of the following letters: SEND(s) | SEND FILE(f) | RECIEVE(r) | EXIT(e)\n");      //file samostatne?
+    printf("Select mode by typing one of the following letters: SEND(s) | RECIEVE(r) | EXIT(e)\n");      //file samostatne?
     char *ip = malloc(iplen * sizeof(char));                                                        //ip address
-    int port;                                                                                       //port
+    int port;                                                                                             //port
     scanf("%c", &mode);
-    switch(mode){                                                                                   //switch menu
+    switch(mode){                                                                                         //switch menu
         case 's':
             printf("Sending mode\n");
             printf("Choose target IP address: ");
             memset(ip, 0, iplen);
             scanf("%s", ip);
-            printf("Target IP address is: %s", ip);
+            printf("Target IP address is: %s\n", ip);
+            printf("Choose target port: ");
+            scanf("%d", &port);
+            client(ip, port);
             break;
         case 'r':
             printf("Receiving mode\n");
